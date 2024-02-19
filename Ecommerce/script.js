134ae11902aefba88e5a86ef0450735e80d4d746
sanasham <sayyedesubvali@gmail.com>
2024-02-19 20:36:54 +0530

    ecommerce website response added

---
+++ Ecommerce/script.js
@@ -0,0 +1,17 @@
+// const bar = document.getElementById("#bar");
+// const close = document.getElementById("#close");
+// const nav = document.getElementById("navbar");
+// // nav.attributes('display')='none';
+// console.log("test click", bar);
+// if (bar) {
+//   console.log("inside bar");
+//   bar.addEventListener("click", () => {
+//     bar.classList.add("active");
+//   });
+// }
+// if (close) {
+//   console.log("inside close");
+//   close.addEventListener("click", () => {
+//     bar.classList.remove("active");
+//   });
+// }
